9f98de2b54ebd930d8de3075a58de38b765dc9fc
Manasi Muluk <86610750+manasi-muluk@users.noreply.github.com>
2023-07-28 08:48:45 +0530

    Update Operations.java

--- src/excellsheet/Operations.java
+++ src/excellsheet/Operations.java
@@ -10,3 +10,10 @@ public interface Operations {
 	public void writeCountToExcel();
 	
 }
+/*
+I am Mans Mul, a recent graduate from MIT with a major in Information Technology. I am excited to share that I have recently joined BARC (Bhabha Atomic Research Centre) as a Graduate Analyst. My journey with BARC began last year when I served as an intern in the cDD (Cyber Defense Department) department.
+
+Beyond my passion for technology and information systems, I have a deep love for the outdoors and enjoy trekking whenever I get the chance. Another pastime that keeps me engaged is solving word puzzles, which I find both intellectually stimulating and fun.
+
+I am looking forward to utilizing my skills and knowledge to contribute to BARC's initiatives and growth, and I'm eager to learn and grow further in my role as a Graduate Analyst. If anyone shares similar interests or wants to chat about technology, trekking, or word puzzles, feel free to reach out!
+*/
