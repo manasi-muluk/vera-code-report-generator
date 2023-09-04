f2b374f9b7d9b83bc502cd86350fe75011a5bf69
Manasi Muluk <86610750+manasi-muluk@users.noreply.github.com>
2023-09-04 19:36:40 +0530

    Update Operations.java

--- src/excellsheet/Operations.java
+++ src/excellsheet/Operations.java
@@ -10,4 +10,67 @@ public interface Operations {
 	public void writeCountToExcel();
 	
 }
+import React, { useRef } from 'react';
+
+function MyForm() {
+  const titleInputRef = useRef(null);
+  const firstNameInputRef = useRef(null);
+  const lastNameInputRef = useRef(null);
+
+  const scrollToInput = (ref) => {
+    ref.current.scrollIntoView({ behavior: 'smooth' });
+    // You can also add additional logic to set focus on the input field here if needed
+  };
+
+  const handleTitleErrorClick = () => {
+    scrollToInput(titleInputRef);
+  };
+
+  const handleFirstNameErrorClick = () => {
+    scrollToInput(firstNameInputRef);
+  };
+
+  const handleLastNameErrorClick = () => {
+    scrollToInput(lastNameInputRef);
+  };
+
+  return (
+    <div>
+      <form>
+        <div>
+          <label htmlFor="title">Title</label>
+          <input
+            type="text"
+            id="title"
+            ref={titleInputRef}
+            // Other input props
+          />
+        </div>
+        {/* Similar divs for other input fields */}
+
+        <Alert warning={
+          <ul>
+            {titleInputIsInvalid && (
+              <li>
+                <a onClick={handleTitleErrorClick}>Title is mandatory</a>
+              </li>
+            )}
+            {FirstNameInputIsInvalid && (
+              <li>
+                <a onClick={handleFirstNameErrorClick}>First name is mandatory</a>
+              </li>
+            )}
+            {LastNameInputIsInvalid && (
+              <li>
+                <a onClick={handleLastNameErrorClick}>Last name is mandatory</a>
+              </li>
+            )}
+          </ul>
+        } />
+      </form>
+    </div>
+  );
+}
+
+export default MyForm;
 
