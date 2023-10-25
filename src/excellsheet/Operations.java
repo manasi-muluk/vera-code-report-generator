4e712bc559f57df632379cc15f275736321036a9
Manasi Muluk <86610750+manasi-muluk@users.noreply.github.com>
2023-10-25 21:02:31 +0530

    Update Operations.java

--- src/excellsheet/Operations.java
+++ src/excellsheet/Operations.java
@@ -74,33 +74,466 @@ function MyForm() {
 
 export default MyForm;
 
-return Optional.ofNullable(responseWrapperDto)
-                .map(ApiResponseDataDto::getData)
-                .map(data -> {
-                    MyAttribute myAttribute = this.objectMapper.convertValue(data.getAttributes(), MyAttribute.class);
-                    return Optional.ofNullable(myAttribute.getEntities())
-                            .orElseGet(List::of)
-                            .stream()
-                            .map(entity -> {
-                                My my = new My();
-                                my.setEntities(entity.getRecords().stream()
-                                        .map(record -> {
-                                            MyEntities myEntities = new MyEntities();
-                                            myEntities.setName(entity.getName());
-                                            myEntities.setRecords(record.getAttributes().stream()
-                                                    .filter(attr -> "MY_REASON_DESCRIPTION".equalsIgnoreCase(attr.getKey()))
-                                                    .map(attr -> {
-                                                        MyEntitiesRecordsInfo reasonInfo = new MyEntitiesRecordsInfo();
-                                                        reasonInfo.setKey(attr.getKey());
-                                                        reasonInfo.setValue(attr.getValue());
-                                                        return reasonInfo;
-                                                    })
-                                                    .collect(Collectors.toList()));
-                                            return myEntities;
-                                        })
-                                        .collect(Collectors.toList()));
-                                return my;
+Default (GPT-3.5)
+
+User
+Following files are from "api bean" package
+MyAttribute.java
+package com.example.demo.beans.api;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class MyAttribute {
+    private List<MyAttributeEntities>entities;
+}
+
+
+MyAttributeEntities.java
+package com.example.demo.beans.api;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class MyAttributeEntities {
+    private String name;
+    private List<MyEntitiesRecords> records;
+}
+
+
+MyEntitiesRecords.java
+package com.example.demo.beans.api;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class MyEntitiesRecords {
+    private String id;
+    private String status;
+    private List<MyEntitiesRecordsAttributes> attributes;
+}
+
+
+MyentitiesRecordsAttributes.java
+package com.example.demo.beans.api;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class MyEntitiesRecordsAttributes {
+    private String key;
+    private String value;
+}
+
+Following files are from "presentation bean" package
+My.java
+package com.example.demo.beans.presentation;
+
+import com.example.demo.beans.api.MyAttributeEntities;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Builder
+public class My {
+    private List<MyEntities> entities;
+}
+
+
+Myentities.java
+package com.example.demo.beans.presentation;
+
+import com.example.demo.beans.api.MyEntitiesRecords;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Builder
+public class MyEntities {
+    private String name;
+    private List<MyRecords> records;
+}
+
+
+MyRecords.java
+package com.example.demo.beans.presentation;
+
+import com.example.demo.beans.api.MyEntitiesRecordsAttributes;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Builder
+public class MyRecords {
+    private String id;
+    private String status;
+    private List<MyEntitiesRecordsInfo> attributes;
+}
+
+
+MyEntitiesrecordsInfo.java
+package com.example.demo.beans.presentation;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Builder
+public class MyEntitiesRecordsInfo {
+    private String key;
+    private String value;
+}
+
+
+ResponseBean.java
+package com.example.demo.beans.presentation;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ResponseBean {
+    private My my;
+}
+
+Following files are from "DTO" package,
+ApiResponseDataDto.java
+package com.example.demo.dto;
+
+public class ApiResponseDataDto<T> {
+    private String id;
+    private String type;
+    private T attributes;
+}
+
+
+ApiResponseWrapperDto.java
+package com.example.demo.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonInclude(value= JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ApiResponseWrapperDto<T> {
+    private ApiResponseDataDto<T> data;
+}
+
+This is my mapper class
+package com.example.demo.mapper;
+
+import com.example.demo.beans.api.MyAttribute;
+import com.example.demo.beans.api.MyEntitiesRecordsAttributes;
+import com.example.demo.dto.ApiResponseDataDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.Collection;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class MyMapper {
+    private ObjectMapper objectMapper;
+
+    public My retriveMyApi(ApiResponseDataDto<MyAttribute>responseWrapperDto)
+    {
+        return Optional.ofNullable(responseWrapperDto).map(data -> data.getData()). map(attr -> {
+            My myapi=null;
+            final MyAttribute myAttribute= this.objectMapper.convertValue(attr.getAttributes(), MyAttribute.class);
+            final Optional<MyEntitiesRecordsAttributes> myEntities= Optional.ofNullable(myAttribute.getEntities()).map(Collection :: stream).orElse(Stream.empty())
+                    .flatMap(ser -> ser.getRecords().stream())
+                    .flatMap(ga -> ga.getAttributes().stream())
+                    .filter(vc -> ! Myconstants.MY_REASON_DESCRIPTION.equalIgnoreCase(vc.getKey()))
+                    sdhApi= Sdh.builder().build();
+                    return sdhApi;
+        })
+    }
+}
+
+
+
+This is the json file from which I am fetching the data,
+{
+  "data": {
+    "id": "",
+    "type": "ResponseEntity",
+    "attributes": {
+      "entities": [
+        {
+          "name": "REASON_01",
+          "records": [
+            {
+              "status": "approved",
+              "attributes": [
+                {
+                  "key": "MY_REASON_CODE",
+                  "value": "MY ERROR"
+                },
+                {
+                  "key": "MY_REASON_DESCRIPTION",
+                  "value": "MY ERROR"
+                }
+              ]
+            },
+            {
+              "status": "approved",
+              "attributes": [
+                {
+                  "key": "MY_REASON_CODE",
+                  "value": "MY goodwill"
+                },
+                {
+                  "key": "MY_REASON_DESCRIPTION",
+                  "value": "MY my_problem"
+                }
+              ]
+            }
+          ]
+        }
+          ]
+        }
+
+    }
+  }
+
+
+This is the response that I want,
+{
+  "resons": [
+    {
+      "reason": "My error"
+    },
+    {
+      "reason": "MY my_problem";
+    }
+  ]
+}
+
+what changes should I made in my mapper class to get the expected result? (the code that I sent you is just overview of the code)
+ChatGPT
+To achieve the desired result, you need to modify your MyMapper class to transform the input JSON data into the expected output. You'll need to iterate through the JSON structure and extract the "MY_REASON_DESCRIPTION" values. Here's a modified version of your MyMapper class to accomplish this:
+
+java
+Copy code
+import com.example.demo.beans.api.MyAttribute;
+import com.example.demo.beans.api.MyEntitiesRecordsAttributes;
+import com.example.demo.dto.ApiResponseDataDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class MyMapper {
+    private ObjectMapper objectMapper;
+
+    public List<String> retrieveReasons(ApiResponseDataDto<MyAttribute> responseWrapperDto) {
+        List<String> reasons = new ArrayList<>();
+
+        Optional.ofNullable(responseWrapperDto)
+            .map(data -> data.getData())
+            .map(attr -> {
+                MyAttribute myAttribute = this.objectMapper.convertValue(attr.getAttributes(), MyAttribute.class);
+                return Optional.ofNullable(myAttribute.getEntities()).orElse(new ArrayList<>())
+                    .stream()
+                    .flatMap(entity -> entity.getRecords().stream())
+                    .flatMap(record -> record.getAttributes().stream())
+                    .filter(attr -> "MY_REASON_DESCRIPTION".equalsIgnoreCase(attr.getKey()))
+                    .map(MyEntitiesRecordsAttributes::getValue)
+                    .forEach(reasons::add);
+            });
+
+        return reasons;
+    }
+}
+In this modified code, the retrieveReasons method extracts the "MY_REASON_DESCRIPTION" values from the input JSON and adds them to a list. You can then use this list to construct the desired output structure.
+
+Please note that the code provided here assumes that you want to retrieve the "MY_REASON_DESCRIPTION" values as a list of strings. If you need to build a different data structure for the response, you can adapt the code accordingly.
+
+
+
+
+User
+I want to get output of type List<My> 
+ChatGPT
+If you want to transform the input JSON into a List<My> structure, you can modify the MyMapper class as follows:
+
+java
+Copy code
+import com.example.demo.beans.api.MyAttribute;
+import com.example.demo.beans.api.MyEntitiesRecords;
+import com.example.demo.beans.api.MyEntitiesRecordsAttributes;
+import com.example.demo.dto.ApiResponseDataDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class MyMapper {
+    private ObjectMapper objectMapper;
+
+    public List<My> retrieveMyList(ApiResponseDataDto<MyAttribute> responseWrapperDto) {
+        List<My> myList = new ArrayList<>();
+
+        Optional.ofNullable(responseWrapperDto)
+            .map(data -> data.getData())
+            .map(attr -> {
+                MyAttribute myAttribute = this.objectMapper.convertValue(attr.getAttributes(), MyAttribute.class);
+                return Optional.ofNullable(myAttribute.getEntities()).orElse(new ArrayList<>())
+                    .stream()
+                    .map(entity -> {
+                        My my = new My();
+                        my.setEntities(entity.getRecords().stream()
+                            .map(record -> {
+                                MyEntities myEntities = new MyEntities();
+                                myEntities.setName(entity.getName());
+                                myEntities.setRecords(record.getAttributes().stream()
+                                    .filter(attr -> "MY_REASON_DESCRIPTION".equalsIgnoreCase(attr.getKey()))
+                                    .map(attr -> {
+                                        MyRecords myRecords = new MyRecords();
+                                        myRecords.setStatus(record.getStatus());
+                                        myRecords.setId(record.getId());
+                                        MyEntitiesRecordsAttributes reasonAttr = new MyEntitiesRecordsAttributes();
+                                        reasonAttr.setKey(attr.getKey());
+                                        reasonAttr.setValue(attr.getValue());
+                                        myRecords.setAttributes(Collections.singletonList(reasonAttr));
+                                        return myRecords;
+                                    })
+                                    .collect(Collectors.toList()));
+                                return myEntities;
                             })
-                            .collect(Collectors.toList());
-                })
-                .orElse(List.of());
+                            .collect(Collectors.toList()));
+                        return my;
+                    })
+                    .forEach(myList::add);
+            });
+
+        return myList;
+    }
+}
+In this modified code, the retrieveMyList method transforms the input JSON into a list of My objects, with each My object containing the desired structure based on the "MY_REASON_DESCRIPTION" values.
+
+Please make sure to adjust the code further if you need additional fields or changes in the structure according to your specific requirements.
+
+
+
+
+User
+
+2 / 2
+
+no instance(s) of type variable(s) exist so that MyEntitiesrecordsAttributes conforms to MyentitiesRecordsInfo inference variable T has incompatible bounds: equality constrains: MyentitiesRecordsInfo lower bounds: MentitiesRecordsAttributesa. Please rewrite the code without giving errors
+ChatGPT
+I apologize for the previous oversight. To fix the compilation error, you can modify the code as follows:
+
+java
+Copy code
+import com.example.demo.beans.api.MyAttribute;
+import com.example.demo.beans.presentation.My;
+import com.example.demo.beans.presentation.MyEntities;
+import com.example.demo.beans.presentation.MyRecords;
+import com.example.demo.beans.presentation.MyEntitiesRecordsInfo;
+import com.example.demo.dto.ApiResponseDataDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class MyMapper {
+    private ObjectMapper objectMapper;
+
+    public List<My> retrieveMyList(ApiResponseDataDto<MyAttribute> responseWrapperDto) {
+        List<My> myList = new ArrayList<>();
+
+        Optional.ofNullable(responseWrapperDto)
+            .map(data -> data.getData())
+            .map(attr -> {
+                MyAttribute myAttribute = this.objectMapper.convertValue(attr.getAttributes(), MyAttribute.class);
+                return Optional.ofNullable(myAttribute.getEntities()).orElse(new ArrayList<>())
+                    .stream()
+                    .map(entity -> {
+                        My my = new My();
+                        my.setEntities(entity.getRecords().stream()
+                            .map(record -> {
+                                MyEntities myEntities = new MyEntities();
+                                myEntities.setName(entity.getName());
+                                myEntities.setRecords(record.getAttributes().stream()
+                                    .filter(attr -> "MY_REASON_DESCRIPTION".equalsIgnoreCase(attr.getKey()))
+                                    .map(reasonAttr -> {
+                                        MyRecords myRecords = new MyRecords();
+                                        myRecords.setStatus(record.getStatus());
+                                        myRecords.setId(record.getId());
+                                        List<MyEntitiesRecordsInfo> attributes = new ArrayList<>();
+                                        MyEntitiesRecordsInfo reasonInfo = new MyEntitiesRecordsInfo();
+                                        reasonInfo.setKey(reasonAttr.getKey());
+                                        reasonInfo.setValue(reasonAttr.getValue());
+                                        attributes.add(reasonInfo);
+                                        myRecords.setAttributes(attributes);
+                                        return myRecords;
+                                    })
+                                    .collect(Collectors.toList()));
+                                return myEntities;
+                            })
+                            .collect(Collectors.toList()));
+                        return my;
+                    })
+                    .forEach(myList::add);
+            });
+
+        return myList;
